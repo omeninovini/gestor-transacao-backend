ce7c7539a78b852793950b8403e49934382049fc
Vinicius Barbosa Martins <vini.martinslol@gmail.com>
2020-06-14 12:15:03 -0300

    Desafio 06, CRUD de transaçoes e cadastro de transaçoes por CSV

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transctionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transctionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transctionRepository.remove(transaction);
   }
 }
 
